a19c5127cff102b7ae83032728aaa0eafefe3e3a
anniepineda <anniepineda@outlook.com>
2019-12-10 13:46:23 -0800

    constructor function

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ app.use(cors());
 //   this.image_url = url;
 // }
 
+function Location (location) {
+  this.formatted_query = location.formatted_address;
+  this.latitude = location.geometry.location.lat
+  this.longitude= location.geometry.location.lng
+
+}
+
 // app.get('/yoda', (request, response) => {
 //   const y = new Yoda('I\'m yoda', 'https://cnet2.cbsistatic.com/img/WbIDMaD6bPQgqHrwITUe5HBx5lo=/756x567/2019/11/19/2eddb56d-56a3-4569-874e-32cd61180d6a/babyyoda2.jpg');
 
@@ -34,7 +41,6 @@ app.use(cors());
 // });
 
 app.get('/location', (req, res) => {
-  // require does NOT take 1 million years
   const geoData = require('./data/geo.json');
   console.log(geoData);
   const firstGeoDataResult = geoData.results[0];
@@ -43,20 +49,19 @@ app.get('/location', (req, res) => {
   console.log(geometry);
   const location = geometry.location;
   console.log(location);
-
+  
   res.send({
     'search_query': 'seattle',
     'formatted_query': 'Seattle, WA, USA',
     'latitude': '47.606210',
-    'longitude': '-122.332071'`
-  } );
-
+    'longitude': '-122.332071'
+  });
 });
 
 
-// app.get('/unicorn', handleUnicornRequest);
-
+// app.get('/unicorn', handleUnicornRequest)
 app.listen(PORT, () => {
-  console.log(`app is up on PORT: ${PORT}`);
-});
+  console.log(`app is running on PORT: ${PORT}`);
 
+
+});
